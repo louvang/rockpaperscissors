3800b3d56ec14baec0fbaaac9150a83e92149095
Lucky <lou@louvang.com>
2020-11-21 18:03:29 -0600

    Release frontend version

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# rockpaperscissors
-A simple Rock-Paper-Scissors game using JavaScript
+# [Rock Paper Scissors](https://voidteddy.github.io/rockpaperscissors/)
+
+This is a website that allows you to click and play Rock Paper Scissors against the "Computer". The prompt comes from [TheOdinProject](https://theodinproject.com/) in an effort to refresh myself with JavaScript and DOM.
+
+<a href="https://voidteddy.github.io/rockpaperscissors/" target="_blank"><img src="https://voidteddy.github.io/rockpaperscissors/img/preview.png" alt="Rock Paper Scissors Preview" width="750px" /></a>
+
+### Gameplay
+
+You, the player, must choose the 'weapon' of your choice to beat the computer. As with all games of Rock Paper Scissors, you never quite know what your opponent will play. It is simply by chance whether or not you will win.
+
+Whichever player reaches 5 points first wins the game. Simply click on the "Play Again" button to start over.
+
+### About Code
+
+This game uses plain vanilla JavaScript. There are two JS files. The `console.js` file loops the game for 5 rounds and logs each play in the console. The working file hooked up to the index page is `script.js` which uses DOM to make changes on the browser.
+
+### Thoughts
+
+This isn't my first time doing a project like this. I've actually done this exact project before although in a much different fashion and when I was less-versed with JavaScript. This time around, I find myself cementing the `addEventListener()` method.
+
+When I started learning React, I had a very basic understanding of `addEventListener()` and the parameters that functioned best with it. This time around, it's easier to understand that the method doesn't need you to call a function.
+
+This is a no-no as it will execute the function as soon as the page loads:
+
+```javascript
+btn.addEventListener("click", alertFunction());
+```
+
+We want the function to execute when the button is clicked. So we need to take out the parentheses as follows:
+
+```javascript
+btn.addEventListener("click", alertFunction);
+```
+
+The second parameter is the 'listener'. It's an object and it's waiting for the button to be clicked. As soon as that happens, the listener fires off what it's been told to do.
+
+Now because this 'listener' is an object and not a function, your parameters will have to be added through `e` aka the event. All you need to do is define a parameter and reference it:
+
+```javascript
+const playPaper = document.querySelector("#paper");
+playPaper.addEventListener("click", playRound);
+playPaper.selection = "paper"; // your parameter
+
+// to access the parameter in your function
+const playRound = (e) => {
+  console.log(e.currentTarget.selection); // logs "paper"
+};
+```
+
+#### Possible Future Projects
+
+I feel like Rock Paper Scissors would be a good way to cement React and its state-changing mechanics. I struggle a bit with React so I may consider making a React version of the game.
---
+++ js/script.js
@@ -0,0 +1,139 @@
+const computerPlay = () => {
+  let compsChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+  switch (compsChoice) {
+    case 1:
+      compsChoice = "rock";
+      break;
+    case 2:
+      compsChoice = "paper";
+      break;
+    case 3:
+      compsChoice = "scissors";
+      break;
+  }
+
+  return compsChoice;
+};
+
+const playRound = (e) => {
+  if (playerScore == 5 || compScore == 5) {
+    return;
+  }
+
+  const playerSelection = e.currentTarget.selection;
+  const computerSelection = computerPlay();
+
+  if (playerSelection == "rock") {
+    yourPlayImg.setAttribute("src", "img/rock.svg");
+    switch (computerSelection) {
+      case "rock":
+        msgDisplay.textContent = "It's a tie.";
+        setCompPlayImg("rock");
+        break;
+      case "paper":
+        setCompPlayImg("paper");
+        compWinRound();
+        break;
+      case "scissors":
+        setCompPlayImg("scissors");
+        playerWinRound();
+        break;
+    }
+  } else if (playerSelection == "paper") {
+    yourPlayImg.setAttribute("src", "img/paper.svg");
+    switch (computerSelection) {
+      case "rock":
+        setCompPlayImg("rock");
+        playerWinRound();
+        break;
+      case "paper":
+        setCompPlayImg("paper");
+        msgDisplay.textContent = "It's a tie.";
+        break;
+      case "scissors":
+        setCompPlayImg("scissors");
+        compWinRound();
+        break;
+    }
+  } else if (playerSelection == "scissors") {
+    yourPlayImg.setAttribute("src", "img/scissors.svg");
+    switch (computerSelection) {
+      case "rock":
+        setCompPlayImg("rock");
+        compWinRound();
+        break;
+      case "paper":
+        setCompPlayImg("paper");
+        playerWinRound();
+        break;
+      case "scissors":
+        setCompPlayImg("scissors");
+        msgDisplay.textContent = "It's a tie.";
+        break;
+    }
+  }
+};
+
+const playerWinRound = () => {
+  msgDisplay.textContent = "You won this round.";
+  playerScore++;
+  playerScoreDisplay.textContent = playerScore;
+  if (playerScore == 5) {
+    msgDisplay2.textContent = "You win!";
+    playAgainBtn.setAttribute("style", "display: block");
+  }
+};
+
+const compWinRound = () => {
+  msgDisplay.textContent = "Computer won this round.";
+  compScore++;
+  compScoreDisplay.textContent = compScore;
+  if (compScore == 5) {
+    msgDisplay2.textContent = "Computer wins!";
+    playAgainBtn.setAttribute("style", "display: block");
+  }
+};
+
+const setCompPlayImg = (play) => {
+  compPlayImg.setAttribute("src", `img/${play}.svg`);
+};
+
+let playerScore = 0;
+let compScore = 0;
+
+const playerScoreDisplay = document.querySelector("#player-score");
+playerScoreDisplay.textContent = playerScore;
+
+const compScoreDisplay = document.querySelector("#comp-score");
+compScoreDisplay.textContent = compScore;
+
+const msgDisplay = document.querySelector(".message");
+msgDisplay.textContent = "Click on an item above to play a round.";
+
+const msgDisplay2 = document.querySelector(".message2");
+const playAgainBtn = document.querySelector("#play-again");
+const yourPlayImg = document.querySelector("#your-play");
+const compPlayImg = document.querySelector("#comp-play");
+
+const playRock = document.querySelector("#rock");
+playRock.addEventListener("click", playRound);
+playRock.selection = "rock";
+
+const playPaper = document.querySelector("#paper");
+playPaper.addEventListener("click", playRound);
+playPaper.selection = "paper";
+
+const playScissors = document.querySelector("#scissors");
+playScissors.addEventListener("click", playRound);
+playScissors.selection = "scissors";
+
+playAgainBtn.addEventListener("click", () => {
+  playerScore = 0;
+  compScore = 0;
+  playerScoreDisplay.textContent = playerScore;
+  compScoreDisplay.textContent = compScore;
+  msgDisplay.textContent = "Click an item above to play a round.";
+  msgDisplay2.textContent = "";
+  playAgainBtn.setAttribute("style", "display: none");
+});
